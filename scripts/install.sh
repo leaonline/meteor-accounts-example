ebf030248b78305cb589eaeb756ba86e7cd8b5aa
Jan Kuester <jkuester@uni-bremen.de>
2019-08-19 09:51:40 +0200

    install script added

---
+++ scripts/install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+PROJECT_ROOT=$(pwd)
+LIB_DIR=${PROJECT_ROOT}/lib
+
+mkdir -p ${LIB_DIR}
+cd ${LIB_DIR}
+git clone git@github.com:leaonline/meteor-accounts-lea.git
+git clone git@github.com:leaonline/oauth2-server.git
+git clone git@github.com:leaonline/meteor-accounts-oauth-lea.git
+git clone git@github.com:leaonline/meteor-accounts-oauth-lea-config-ui.git
+
+cd ${PROJECT_ROOT}/authserver
+METEOR_PACKAGE_DIRS=${LIB_DIR} meteor update --all-packages
+meteor npm install
+
+cd ${PROJECT_ROOT}/resourceserver
+METEOR_PACKAGE_DIRS=${LIB_DIR} meteor update --all-packages
+meteor npm install
