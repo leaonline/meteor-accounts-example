00b8a7fb43ca265ba679cbe10951fd919d4bbe33
Jan Küster <jkuester@uni-bremen.de>
2019-08-21 00:05:37 +0200

    introduced oauth app registration

--- authserver/server/leaOauth.js
+++ authserver/server/leaOauth.js
@@ -1,32 +1,48 @@
-import { Meteor } from "meteor/meteor"
+import { Meteor } from 'meteor/meteor'
 import { OAuth2Server } from 'meteor/leaonline:oauth2-server'
 
-export const oauth2server = new OAuth2Server({
-  serverOptions: {
-    addAcceptedScopesHeader: true,
-    addAuthorizedScopesHeader: true,
-    allowBearerTokensInQueryString: false,
-    allowEmptyState: false,
-    authorizationCodeLifetime: 300,
-    accessTokenLifetime: 3600,
-    refreshTokenLifetime: 1209600,
-    allowExtendedTokenAttributes: false,
-    requireClientAuthentication: true
-  },
-  model: {
-    accessTokensCollectionName: 'oauth_access_tokens',
-    refreshTokensCollectionName: 'oauth_refresh_tokens',
-    clientsCollectionName: 'oauth_clients',
-    authCodesCollectionName: 'oauth_auth_codes',
+const settings = Meteor.settings.oauth
+
+Meteor.startup(() => {
+  const oauth2server = new OAuth2Server({
+    serverOptions: {
+      addAcceptedScopesHeader: true,
+      addAuthorizedScopesHeader: true,
+      allowBearerTokensInQueryString: false,
+      allowEmptyState: false,
+      authorizationCodeLifetime: 300,
+      accessTokenLifetime: 3600,
+      refreshTokenLifetime: 1209600,
+      allowExtendedTokenAttributes: false,
+      requireClientAuthentication: true
+    },
+    model: {
+      accessTokensCollectionName: 'oauth_access_tokens',
+      refreshTokensCollectionName: 'oauth_refresh_tokens',
+      clientsCollectionName: 'oauth_clients',
+      authCodesCollectionName: 'oauth_auth_codes',
+      debug: true
+    },
+    routes: {
+      accessTokenUrl: '/oauth/token',
+      authorizeUrl: '/oauth/authorize',
+      errorUrl: '/oauth/error',
+      fallbackUrl: '/oauth/*'
+    },
     debug: true
-  },
-  routes: {
-    accessTokenUrl: '/oauth/token',
-    authorizeUrl: '/oauth/authorize',
-    errorUrl: '/oauth/error',
-    fallbackUrl: '/oauth/*'
-  }
+  })
+
+
+  oauth2server.registerClient({
+    title: 'my client app',
+    secret: 'supersecret1234',
+    clientApp: 'clientAppId1234',
+    redirectUris: [ 'http://localhost:4000/_oauth/lea' ]
+  })
 })
+
+/*
+oauth2server.app.use('')
 
 // Add a route to return account information
 oauth2server.authenticatedRoute('/oauth/account', function (req, res, next) {
@@ -37,8 +53,4 @@ oauth2server.authenticatedRoute('/oauth/account', function (req, res, next) {
     name: user.name
   })
 })
-
-oauth2server.app.use('*', function (req, res, next) {
-  res.writeHead(404)
-  res.end('route not found')
-})
+*/
--- authserver/server/main.js
+++ authserver/server/main.js
@@ -1,8 +1,6 @@
 import { Meteor } from 'meteor/meteor'
 import { Schema } from '../imports/api/schema'
 
-import { oauth2server } from './leaOauth'
-
 const validateNewUserSchema = Schema.create({
   _id: { type: String },
   username: String,
@@ -35,12 +33,5 @@ const createUserSchema = Schema.create({
 Meteor.methods({
   'createNewUser' ({ username, email, password }) {
     return Accounts.createUser({ username, email, password })
-  },
-  'generateAuthCode' ({ clientId }) {
-    const userId = this.userId
-    if (!userId) throw new Meteor.Error('403', 'permission denied', 'you have to login to obtain an auth code')
-
-    const code = oauth2server.mode.generateAuthorizationCode()
-    console.log(code)
   }
 })
